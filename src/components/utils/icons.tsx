253d0cc70b360dfc511a05497e18558dee973dcd
trocipon <thibaud.rocipon@protonmail.com>
2026-03-03 17:29:52 +0100

    add imports to icons.tsx

--- src/components/utils/icons.tsx
+++ src/components/utils/icons.tsx
@@ -1,4 +1,4 @@
-import { FaGraduationCap, FaQuoteLeft, FaTachometerAlt, FaTasks, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaGraduationCap, FaQuoteLeft, FaTachometerAlt, FaTasks, FaGithub, FaLinkedin, FaBriefcase, FaDownload, FaMapPin, FaMailBulk, FaArrowUp, FaArrowDown, FaPaperPlane } from "react-icons/fa";
 import { MdMonitor, MdFlashOn, MdStorage, MdSpeed, MdRocketLaunch, MdViewKanban } from "react-icons/md";
 
 export const GraduationCap = FaGraduationCap;
@@ -7,6 +7,13 @@ export const Gauge = FaTachometerAlt;
 export const Kanban = FaTasks;
 export const Github = FaGithub;
 export const Linkedin = FaLinkedin;
+export const Briefcase = FaBriefcase;
+export const Download = FaDownload;
+export const MapPin = FaMapPin;
+export const Mail = FaMailBulk;
+export const ArrowUp = FaArrowUp;
+export const ArrowDown = FaArrowDown;
+export const Send = FaPaperPlane;
 
 export const Monitor = MdMonitor;
 export const Zap = MdFlashOn;
